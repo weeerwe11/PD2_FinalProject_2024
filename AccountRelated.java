4bd3b6de44bd2f3beec50c8ac004835565bf2f8f
Sin-Mao Lin <145822308+weeerwe11@users.noreply.github.com>
2024-06-17 21:03:16 +0800

    Update AccountRelated.java

--- AccountRelated.java
+++ AccountRelated.java
@@ -1,36 +1,106 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class AccountRelated {
 
-    public List<String> accountList = new ArrayList<>();
     FileRelated fr = new FileRelated();
 
-    public void set (List<String> accountList) {
-        this.accountList = accountList;
-    }
+    public void SignUp(String name) { //註冊
+        File accListFile = new File("accList.txt");
     
-    public void SignUp(String name) {
-        System.out.println("Already exist");
-    }
-    
-    public void LogIn(String name) {
-        
-        for(String account : accountList) {
-            
-            if(name.equals(account)) {
-                fr.input(account);
-                
+        //不存在就創一個
+        if(!accListFile.exists()) {
+            try {
+                accListFile.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            else {
-                System.out.println("Not exist, please sign up.");
+        }
+    
+        boolean isExist = false;
+    
+        //檢查帳號是否已經存在
+        try(BufferedReader br = new BufferedReader(new FileReader(accListFile))) {
+            String account;
+            while ((account = br.readLine()) != null) {
+                if (name.equals(account)) {
+                    isExist = true;
+                    break;
+                }
+            }
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    
+        if(isExist) {
+            System.out.println("帳號已存在。");
+            System.out.println("登錄成功。");
+        }
+        else {
+            //帳號不存在就加入
+            try(BufferedWriter bw = new BufferedWriter(new FileWriter(accListFile, true))) {
+                bw.write(name);
+                bw.newLine();
+                fr.create(name); //創建對應的diary.txt
+                System.out.println("帳號已創建。");
+            } catch(IOException e) {
+                e.printStackTrace();
             }
         }
     }
+    
+    
+    public void LogIn(String name) { //登入
+        File accListFile = new File("accList.txt");
+    
+        //不存在就創一個
+        if(!accListFile.exists()) {
+            try {
+                accListFile.createNewFile();
+            } catch(IOException e) {
+                e.printStackTrace();
+            }
+        }
+    
+        boolean isExist = false;
+    
+        //檢查帳號是否存在
+        try(BufferedReader br = new BufferedReader(new FileReader(accListFile))) {
+            String account;
+            while((account = br.readLine()) != null) {
+                if(name.equals(account)) {
+                    isExist = true;
+                    break;
+                }
+            }
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    
+        if(!isExist) {
+            System.out.println("帳號不存在，請註冊。");
+        }
+        else {
+            System.out.println("登錄成功。");
+        }
+    }
+    
+    public Boolean exist(String name) { //檢查帳號是否已經存在
+        File accListFile = new File("accList.txt");
+        boolean isExist = false;
+    
+        //檢查帳號是否已經存在
+        try(BufferedReader br = new BufferedReader(new FileReader(accListFile))) {
+            String account;
+            while ((account = br.readLine()) != null) {
+                if (name.equals(account)) {
+                    isExist = true;
+                    break;
+                }
+            }
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
 
-    public Boolean exist(String name) {
-        
-        return true;
+        return isExist;
     }
 }
